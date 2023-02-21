5e68796ac7cb0c4878491fd07fcf94e44593b669
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-02-21 20:36:27 +0100

    fixtures exemplo

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  def full_name
+    "Sra. #{name}"
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :customers
+  it 'have a name and a email' do
+    customer = customers(:juliana)
+    expect(customer.full_name).to eq('Sra. Juliana Arrighi')
+  end
 end
