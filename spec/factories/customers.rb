86a292c66d7a19326fab42632ae0d0854cf350da
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-05-05 17:53:34 +0200

    has_many

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :customer, aliases: [:user] do
     transient do
       upcased { false }
+      qtt_order { 3 }
     end
 
     name { Faker::Name.name }
@@ -27,6 +28,13 @@ FactoryBot.define do
       gender { 'M' }
     end
 
+    trait :with_order do
+      after(:create) do | customer, evaluator |
+        create_list(:order, evaluator.qtt_order, customer: customer)
+      end
+    end
+
+    factory :customer_with_order, traits: [:with_order]
     factory :customer_female, traits: [:female]
     factory :customer_male, traits: [:male]
     factory :customer_default_female, traits: [:female, :default]
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,14 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  it 'create order with costumer reference' do
+  it 'belongs_to - create order with costumer reference' do
     order = create(:order)
     expect(order.customer).to be_kind_of(Customer)
   end
 
-  it 'create list of 5 orders' do
+  it 'create_list - 5 orders' do
     orders = create_list(:order, 5)
     puts orders.inspect
     expect(orders.count).to eq(5)
+  end
+
+  it 'has_many' do
+    # customer = create(:customer, :with_order, qtt_order: 5)
+    customer = create(:customer_with_order)
+    puts customer.orders.inspect
+    puts customer.inspect
+    expect(customer.orders.count).to eq(3)
   end
 end
