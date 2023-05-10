f1ac16e334f2a0fb7edf90cf983d275157fd338c
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-05-10 15:50:42 +0200

    sensitive data

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -2,9 +2,8 @@ describe 'HTTParty' do
   it 'test request', :vcr do
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #   to_return(status: 200, body: "", headers: {'content-type': 'application/json'})
-      response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
-      content_type = response.headers['content-type']
-      expect(content_type).to match(/application\/json/)
-
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
   end
 end
