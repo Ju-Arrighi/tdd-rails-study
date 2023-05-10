9079877304ee5f9c28b786d4394609ae99d5012e
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-05-10 16:28:15 +0200

    VCR not deterministic variables

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,8 +1,8 @@
 describe 'HTTParty' do
-  it 'test request', :vcr do
+  it 'test request', vcr: { cassette_name: 'HTTParty/test_request', match_requests_on: [:body] } do
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #   to_return(status: 200, body: "", headers: {'content-type': 'application/json'})
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/4')
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
   end
